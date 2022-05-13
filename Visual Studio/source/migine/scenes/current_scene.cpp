96d8db533a0b868579f3e126c407bdcd3f3de3c5
MihneaS <mihnea.dsd.serban@gmail.com>
2022-05-13 16:48:11 +0300

    deleted all_scenes.h file

--- Visual Studio/source/migine/scenes/current_scene.cpp
+++ Visual Studio/source/migine/scenes/current_scene.cpp
@@ -1,5 +1,13 @@
 #include "current_scene.h"
-#include <migine/scenes/all_scenes.h>
+
+#include "Scene_01.h"
+#include "Scene_02.h"
+#include "Scene_03.h"
+#include "Scene_04.h"
+#include "Scene_05.h"
+#include "Scene_06.h"
+#include "Scene_07.h"
+#include "Scene_08.h"
 
 namespace migine {
     Scene_base& migine::get_current_scene() {
